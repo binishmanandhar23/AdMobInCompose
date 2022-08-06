550b3e1f5d41b9c052fe8390949eb1d95e88c95e
binishmanandhar23 <binish.manandhar23@gmail.com>
2022-08-06 10:00:54 +0545

    Interstitial Ad integration

--- AdMobInCompose/src/main/java/io/github/binishmanandhar23/admobincompose/components/interstitial/InterstitialAds.kt
+++ AdMobInCompose/src/main/java/io/github/binishmanandhar23/admobincompose/components/interstitial/InterstitialAds.kt
@@ -1,2 +1,40 @@
 package io.github.binishmanandhar23.admobincompose.components.interstitial
 
+import android.app.Activity
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
+import io.github.binishmanandhar23.admobincompose.utils.LibraryUtils.getActivity
+
+@Composable
+fun rememberInterstitialAdsState(adUnit: String,onAdLoaded:(() -> Unit)? = null, onAdLoadFailed: ((adError: LoadAdError) -> Unit)? = null) =
+    LocalContext.current.getActivity()?.let {
+        InterstitialAdsState(activity = it, adUnit = adUnit, onAdLoadFailed = onAdLoadFailed, onAdLoaded = onAdLoaded)
+    }
+
+class InterstitialAdsState(private val activity: Activity, adUnit: String,onAdLoaded:(() -> Unit)?, onAdLoadFailed: ((adError: LoadAdError) -> Unit)?){
+    private var mInterstitialAd: InterstitialAd? = null
+    init {
+        val adRequest = AdRequest.Builder().build()
+        InterstitialAd.load(
+            activity,
+            adUnit,
+            adRequest,
+            object : InterstitialAdLoadCallback() {
+                override fun onAdFailedToLoad(adError: LoadAdError) {
+                    mInterstitialAd = null
+                    onAdLoadFailed?.invoke(adError)
+                }
+
+                override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                    mInterstitialAd = interstitialAd
+                    onAdLoaded?.invoke()
+                }
+            })
+    }
+
+    fun show() = mInterstitialAd?.show(activity)
+}
--- AdMobInCompose/src/main/java/io/github/binishmanandhar23/admobincompose/utils/LibraryUtils.kt
+++ AdMobInCompose/src/main/java/io/github/binishmanandhar23/admobincompose/utils/LibraryUtils.kt
@@ -1,4 +1,16 @@
 package io.github.binishmanandhar23.admobincompose.utils
 
-object LIbrayUtils {
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
+import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+
+object LibraryUtils {
+    fun Context.getActivity(): Activity? = when (this) {
+        is ComponentActivity -> this
+        is AppCompatActivity -> this
+        is ContextWrapper -> baseContext.getActivity()
+        else -> null
+    }
 }
